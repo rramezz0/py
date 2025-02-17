32efd0771bd1c1d96b2c3caf49a80bc934e69d90
rramezz0 <ramezelshaboury@gmail.com>
2025-02-17 16:51:13 +0200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,34 @@
+x = int(input ("enter 1st number"))
+ 
+y = int(input ("enter 2sec number"))
+
+operator = (input ("enter sign"))
+
+result = ()
+
+if operator == "+" :
+    result = x + y
+    print (f"result = {result}")
+
+elif operator == "-" :
+    result = x-y
+    print(f"result = { result}")
+
+elif operator =="/":
+    result = x/y
+    print (f"result = {result}")
+
+elif operator == "*":
+
+    result = x * y 
+    print (f"result = {result}")
+
+    
+
+   
+
+
+
+
+
+
